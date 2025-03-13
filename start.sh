56046f612f20568ed16cf52704f4f3ca97405c4d
MOVZX <movzx@yahoo.com>
2025-03-13 20:33:29 +0700

    Initial commit
    Signed-off-by: MOVZX <movzx@yahoo.com>

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+readonly DIR="/multimedia/AI/daaandy";
+readonly VENV_DIR="$DIR/venv";
+
+if [[ ! -d "$VENV_DIR" ]]; then
+    echo "Error: Virtual environment directory '$VENV_DIR' not found.";
+
+    exit 1;
+fi
+
+source "$VENV_DIR/bin/activate" ||
+{
+    echo "Failed to activate virtual environment.";
+
+    exit 1;
+}
+
+cd $DIR
+python -m http.server 8000
